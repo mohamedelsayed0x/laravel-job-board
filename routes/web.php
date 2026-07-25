9278348f496d38aea86ae68dfd44f8db363611fe
Mohamed Elsayed <moelsayko@gmail.com>
2026-07-25 22:29:37 +0300

    Add Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterdUserController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,8 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisterdUserController::class, 'create']);
 
 //==========================================
 Route::view('/index', 'index');

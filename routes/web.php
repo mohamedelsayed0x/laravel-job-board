4db7f823d95c6ebebb0e3508ed2f7b883b8d8fca
Mohamed Elsayed <moelsayko@gmail.com>
2026-07-27 05:27:37 +0300

    Authorization Middleware

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,34 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class);
+Route::get('/jobs', [JobController::class, 'index']);
 
+Route::get('/jobs/create', [JobController::class, 'create']);
+
+Route::post('/jobs', [JobController::class, 'store'])
+  ->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+  ->middleware('auth')
+  ->can('edit-job', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update'])
+  ->middleware('auth')
+  ->can('edit-job', 'job');
+
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])
+  ->middleware('auth')
+  ->can('edit-job', 'job');
+
+// Auth
 Route::get('/register', [RegisterdUserController::class, 'create']);
 Route::post('/register', [RegisterdUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
-
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
-
 Route::post('/logout', [SessionController::class, 'destroy']);
+
+
 //==========================================
 Route::view('/index', 'index');
